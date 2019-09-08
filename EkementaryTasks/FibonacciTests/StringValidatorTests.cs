008c3a9cba0bc7804eff4a8b2e388139f0c9a833
Valeriy <farik.dp.2008@gmail.com>
2019-09-09 00:29:33 +0300

    StringValidatorTests.cs

--- EkementaryTasks/FibonacciTests/StringValidatorTests.cs
+++ EkementaryTasks/FibonacciTests/StringValidatorTests.cs
@@ -8,5 +8,25 @@ namespace FibonacciTests
 {
     class StringValidatorTests
     {
+        [Fact]
+        public void Can_Validate_Only_Digits()
+        {
+            //Arrange 
+
+            INumberParser Parser = new Parser();
+
+            var digitsWithChars = new[] { "b45y7ikj8", "b0po" };
+
+            //Act
+
+            bool isExtracted;
+
+            var result = Parser.ExtractDigits(digitsWithChars, out isExtracted);
+
+            //Assert
+            Assert.True(isExtracted);
+            Assert.Equal(4578D, result[0]);
+            Assert.Equal(1D, result[1]);
+        }
     }
 }
